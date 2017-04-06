9e10122afb0e069cb74f0d7aaeb254417c236057
MalhotR1 <rajatmalhotra26@gmail.com>
2017-04-06 10:57:56 +0530

    Module two completed

---
+++ CSharpFundamentals/Grade.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFundamentals {
+    class GradeBook {
+        List<float> grades;
+
+        public GradeBook() {
+            grades = new List<float>();
+        }
+
+        public void AddGrade(float grade) {
+            grades.Add(grade);
+        }
+
+        public GradeStatistics ComputeStatistics() {
+            GradeStatistics stats = new GradeStatistics();
+
+            float sum = 0;
+
+            foreach (float item in grades) {
+                sum += item;
+            }
+
+            stats.AverageGrade = sum / grades.Count;
+            grades.Sort();
+            stats.LowestGrade = grades[0];
+            stats.HighestGrade = grades[grades.Count - 1];
+
+            return stats;
+        }
+    }
+}
--- CSharpFundamentals/Program.cs
+++ CSharpFundamentals/Program.cs
@@ -1,13 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CSharpFundamentals {
-    class Program {
+    public class Program {
         static void Main(string[] args) {
-            Console.WriteLine(args[0]);
+            Console.WriteLine("Hours: ");
+            //int hours = Convert.ToInt32(Console.ReadLine());
+            //int hours = int.Parse(Console.ReadLine());
+            //if (hours < 8) {
+            //    Console.WriteLine("Sleep more!");
+            //} else {
+            //    Console.WriteLine("Lucky bastard..");
+            //}
+
+            GradeBook gradeBook = new GradeBook();
+            gradeBook.AddGrade(90);
+            gradeBook.AddGrade(97.9f);
+            gradeBook.AddGrade(70.7f);
+
+            GradeStatistics stats = new GradeStatistics();
+            stats = gradeBook.ComputeStatistics();
+            Console.WriteLine(stats.HighestGrade);
+            Console.WriteLine(stats.LowestGrade);
+            Console.WriteLine(stats.AverageGrade);
         }
     }
 }
